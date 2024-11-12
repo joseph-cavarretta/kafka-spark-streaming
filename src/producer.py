3e769ccc57e370e4950a901fc532c6320b7f6765
joseph-cavarretta <josephmc241@gmail.com>
2024-11-12 15:57:41 -0700

    finished 1st iteration of producer class and main script

--- src/producer.py
+++ src/producer.py
@@ -1,27 +1,34 @@
+import time
+import uuid
+import random
 from confluent_kafka import avro
 from confluent_kafka.avro import CachedSchemaRegistryClient
 from confluent_kafka.avro import AvroProducer
 
 
-class MockProducer:
+class MockAvroProducer:
     def __init__(self, config_path, topic):
         self.config_path = config_path
         self.topic = topic
         self.config = self.__get_config()
+        self.schema_client = self.__get_schema_client()
         self.schema_id = self.__register_schema()
         self.schema = self.__get_schema()
+
 
     def __get_config(self):
         pass
 
-    def __register_schema(self, schema_client, config):
-        # Create a client instance
-        client = CachedSchemaRegistryClient({'url': config['schema_registry_url']})
 
+    def __get_schema_client(self):
+        return CachedSchemaRegistryClient({'url': self.config['schema_registry_url']})
+    
+
+    def __register_schema(self):
         with open(self.config_path, "r") as f:
             schema_str = f.read()
             schema = avro.loads(schema_str)
-        schema_id = schema_client.register_schema(self.topic, schema)
+        schema_id = self.schema_client.register_schema(self.topic, schema)
         return schema_id
 
 
@@ -29,9 +36,21 @@ class MockProducer:
         return schema_client.get_schema(schema_id)
 
 
-    def get_producer(self, schema_client, schema, config):
+    def avro_producer(self):
         return AvroProducer(
-            {'bootstrap.servers': config['bootstrap.servers']},
-            schema_registry = schema_client,
-            default_value_schema = schema
+            {'bootstrap.servers': self.config['kafka_broker_url']},
+            schema_registry = self.schema_client,
+            default_value_schema = self.schema
         )
+    
+    
+    def generate_data(self, i):
+        devices = ['mobile', 'tablet', 'laptop']
+        events = ['click', 'pageview', 'login', 'download']
+        return {
+            'event_timestamp': time.time(),
+            'event_id': str(i),
+            'event_type': random.choice(events),
+            'device_type': random.choice(devices),
+            'user_id': str(uuid.uuid4())
+        }
--- src/publish.py
+++ src/publish.py
@@ -1,18 +1,7 @@
 import sys
 import time
 import logging
-
-
-schema_registry_url = 'https://172.18.0.4:8081'
-kafka_broker_url = 'https://172.18.0.3:9092'
-# Kafka topic
-topic = 'avro-topic'
-
-# Kafka and Schema Registry configuration
-config = {
-    'bootstrap.servers': kafka_broker_url,
-    'schema.registry.url': schema_registry_url
-}
+import producer
 
 logger = logging.getLogger(__name__)
 
@@ -21,29 +10,6 @@ logging.basicConfig(
     format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     stream=sys.stdout)
-
-
-def get_config():
-    pass
-
-def register_schema(schema_client):
-    with open("events.avsc", "r") as f:
-        schema_str = f.read()
-        schema = avro.loads(schema_str)
-    schema_id = schema_client.register_schema('test-topic', schema)
-    return schema_id
-
-
-def get_schema(schema_client, schema_id):
-    return schema_client.get_schema(schema_id)
-
-
-def get_producer(schema_client, schema, config):
-    return AvroProducer(
-        {'bootstrap.servers': config['bootstrap.servers']},
-        schema_registry = schema_client,
-        default_value_schema = schema
-    )
 
 # Delivery report callback
 def delivery_report(err, msg):
@@ -55,16 +21,16 @@ def delivery_report(err, msg):
 
 
 if __name__ == '__main__':
-    
-    
-    # Produce Avro messages
+    client = producer.MockAvroProducer()
+    avro_producer = client.avro_producer()
+
+    # Produce messages
     for i in range(1000):
-        value = {'id': i, 'name': f'User {i}', 'email': None if i % 2 == 0 else f'user{i}@example.com'}
-        
+        record = client.generate_data(i)
         # Send the message using Avro serialization
         avro_producer.produce(
-            topic=topic,
-            value=value,
+            topic=client.topic,
+            value=record,
             callback=delivery_report
         )
         time.sleep(2)
